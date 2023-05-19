d4405a2c5f25ada2775a9ba18eb7ad992c86e58d
maulie5 <83541036+maulie5@users.noreply.github.com>
2023-05-19 18:01:42 +0200

    Update CCTweaked.lua

--- CCTweaked.lua
+++ CCTweaked.lua
@@ -9,7 +9,7 @@ local function activateReactor(status)
     
 end
 while true do 
-    if reactor.getTemperature() =< 1000 and reactor.getCoolantFilledPercentage >= 0.4 and reactor.getWasteFilledPercentage() <= 0.7 and reactor.getDamagePercent() == 0 and reactor.getFuelFilledPercentage() >= 0.2 and reactor.getHeatedCoolantFilledPercentage() <= 0.8 then
+    if reactor.getTemperature() <= 1000 and reactor.getCoolantFilledPercentage >= 0.4 and reactor.getWasteFilledPercentage() <= 0.7 and reactor.getDamagePercent() == 0 and reactor.getFuelFilledPercentage() >= 0.2 and reactor.getHeatedCoolantFilledPercentage() <= 0.8 then
         status = true;
     else 
         status = false;
