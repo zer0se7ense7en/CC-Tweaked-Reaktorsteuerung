21331f5d5cf57b63f966b0973a19461ef0047153
maulie5 <maulie5@proton.me>
2023-05-19 16:36:32 +0200

    Create CCTweaked.lua

---
+++ CCTweaked.lua
@@ -0,0 +1,23 @@
+local reactor = peripheral.wrap("fissionReactorLogicAdapter_1")
+local reactorStatus = true ;
+local function activateReactor(status)
+    if status then
+        reactor.activate();
+    else 
+        reactor.scram();
+    end
+    
+end
+while true do 
+    
+    if reactor.getTemperature() <= 1100 then
+        reactorStatus = true;
+        print("Temperature is ok!!");
+
+    else
+        reactorStatus = false ;
+       print("Temperature is too high!!!");
+
+    end
+    activateReactor(reactorStatus);
+end
